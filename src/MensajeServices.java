f45e392de55817fc0f2207683aede4894bdcba7e
LAPTOP-T3BCHV0Q\YEISSON <yeissonozy@hotmail.com>
2023-04-07 23:31:38 -0500

    EnviandoPrimerMensaje

--- src/MensajeServices.java
+++ src/MensajeServices.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class MensajeServices {
     public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu Mensaje");
+        String mensaje = sc.nextLine();
 
+        System.out.println("Tu Nombre");
+        String nombre = sc.nextLine();
+
+        Mensaje registro = new Mensaje();
+        registro.setMensaje(mensaje);
+        registro.setAutorMensaje(nombre);
+        MensajesDAO.crearMensajeDB(registro);
     }
 
     public static void listarMensaje(){
--- src/MensajesDAO.java
+++ src/MensajesDAO.java
@@ -1,6 +1,26 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MensajesDAO {
     public static void crearMensajeDB(Mensaje mensaje){
+        Conexion dbConnect = new Conexion();
+        try(Connection conexion = dbConnect.getConnection()){
+            PreparedStatement ps = null;
+            try{
+                String query = "INSERT INTO mensajes (mensaje, autor_mensaje) VALUES (?,?)";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1,mensaje.getMensaje());
+                ps.setString(2,mensaje.getAutorMensaje());
+                ps.executeUpdate();
+                System.out.println("mensaje creado");
+            }catch (SQLException ex){
+                System.out.println(ex);
 
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
     }
 
     public static void leerMensajeDB(){
